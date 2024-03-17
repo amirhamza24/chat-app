bef57dc532f4e107064b8012ad1b2f3ed52bb47d
amirhamza24 <ridoykhan7897890@gmail.com>
2024-03-17 16:20:38 +0600

    server side connect

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req, res) => {
+  res.send("signup users");
+  console.log("signup user");
+}
+
+export const login = (req, res) => {
+  console.log("login user");
+}
+
+export const logout = (req, res) => {
+  console.log("logout user");
+}
