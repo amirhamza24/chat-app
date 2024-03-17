8d6350f7110c91c5d26d89fb50b3d18102677b72
amirhamza24 <ridoykhan7897890@gmail.com>
2024-03-17 20:22:02 +0600

    user created in db using server side

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
   // root route http://localhost:5000/
-  res.send("Hello World!!!!");
-})
-
-app.use("/api/auth", authRoutes)
+  // res.send("Hello World!!!!");
+// })
 
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port: ${PORT}`)
+});
