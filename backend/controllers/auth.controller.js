2dee690fd06b685153c47ef160bdbb9cc6ba706b
amirhamza24 <ridoykhan7897890@gmail.com>
2024-03-23 03:09:20 +0600

    conversation, message send

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -65,7 +65,7 @@ export const login = async (req, res) => {
     if(!user || !isPasswordCorrect) {
       return res.status(400).json({error: "Invalid username or password"});
     }
-
+    
     generateTokenAndSetCookie(user._id, res);
 
     res.status(200).json({
